73d0a62a25015a95a0c5721b555889c64ab2e61a
hermkan <hermann.kanga.dev@gmail.com>
2022-07-19 16:02:11 -0400

    added fetch

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import useInvoices from '../hooks/useInvoices';
 
 function Home() {
-  // const invoices = useInvoices();
+  const invoices = useInvoices();
   return (
     <div>
-      {/* {invoices.map((invoice) => (
+      {invoices.map((invoice) => (
         <p key={invoice.id}>{invoice.content.clientName}</p>
-      ))} */}
+      ))}
       test heroku
     </div>
   );
