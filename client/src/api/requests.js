a2c3b60f5d09c1289a3876d816670f220d59b8d5
hermkan <hermann.kanga.dev@gmail.com>
2022-07-19 15:34:26 -0400

    updated endpoint fe

--- client/src/api/requests.js
+++ client/src/api/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-// const API_URL = 'https://invoicesv1.herokuapp.com/';
+const API_URL = 'https://invoicesv1.herokuapp.com/';
 // const API_URL = 'http://localhost:8000/v1/invoices';
-const API_URL = 'v1/invoices';
+// const API_URL = 'v1/invoices';
 
 // Load invoices
 async function httpGetInvoices() {
