f79890c50e3e4808d342731be5b2eddec5cc3dee
hermkan <hermann.kanga.dev@gmail.com>
2022-09-09 07:41:28 -0400

    rebuildv2

--- client/src/api/requests.js
+++ client/src/api/requests.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 // const API_URL = "https://invoicev1.herokuapp.com/v1/invoices";
 //dev
-export const API_URL = "http://localhost:8000/v1/invoices";
+// export const API_URL = "http://localhost:8000/v1/invoices";
 //prod
-// const API_URL = 'v1/invoices';
+const API_URL = "v1/invoices";
 
 // Load invoices
 async function httpGetInvoices() {
